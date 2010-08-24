83dabe3d220fa5d8f54bd14df9c6cb7d01216e7d
Andy <andy@onthewings.net>
2010-08-24 15:17:12 +0800

    updated to 1.5.1

--- Raphael.hx
+++ Raphael.hx
@@ -97,6 +97,7 @@ extern class Raphael {
 	static public var type(default, null):String; //"SVG", "VML"
 	static public var version(default, null):String;
 	public var customAttributes:Dynamic;
+	static public function angle(x1:Float, y1:Float, x2:Float, y2:Float, ?x3:Float, ?y3:Float):Float;
 }
 
 extern class RaphaelElement {
